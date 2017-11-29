9b17eacd890c705cf6f480ea1f65303c58eac2af
Alex Palma <alex-palma@outlook.com>
2017-11-30 10:32:44 +1100

    Add code for app STATE

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import StockInfo from './components/StockInfo';
 import './App.css';
 
 class App extends Component {
+  state = {
+    quote: {
+      symbol: 'NFLX',
+      companyName:'Netflix inc',
+      primaryExchange:'Nasdaq Global Select',
+      latestPrice:188.15,
+      latestSource:'Close',
+      week52high:188.15,  
+      week52low:188.15
+    }
+  }
+
   render() {
     return (
       <div className="App">
